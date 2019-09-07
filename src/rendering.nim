16220cf53d23fe8579907c60dd61547b0975a311
Dmitry Matveyev <dm.matveyev@protonmail.com>
2019-09-07 23:15:32 +0300

    Add hash to button hrefs

--- src/rendering.nim
+++ src/rendering.nim
@@ -9,7 +9,7 @@ type Choice* = object
 proc renderChoices*(choices: openArray[Choice]): VNode =
   result = buildHtml(tdiv(class="buttons are-medium is-centered")):
     for chs in choices:
-      a(class="button is-primary is-outlined", href=chs.path):
+      a(class="button is-primary is-outlined", href="#"&chs.path):
         text chs.title
 
 template withLayout*(content: untyped) =
