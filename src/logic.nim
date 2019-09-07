9cf36486ca2bc227690bfefa8e73ce49a1936cb9
Dmitry Matveyev <dm.matveyev@protonmail.com>
2019-09-08 00:58:18 +0300

    Add basic functionality

--- src/logic.nim
+++ src/logic.nim
@@ -1,12 +1,12 @@
-import tables
+import tables, strutils
 
 include karax / prelude
 import rendering
 
 type
   QuestionScreen = object
-    title: kstring
-    choices: seq[Choice]
+    title*: kstring
+    choices*: seq[Choice]
 
 const
   questionScreens = {
@@ -14,11 +14,19 @@ const
         title: "What is your purpose?",
         choices: @[Choice(title: kstring"I'm here to kill you", path: kstring"killing"),
                    Choice(title: kstring"I'm aftaid", path: kstring"afraid")]
+      ),
+    "killing": QuestionScreen(
+        title: "How bad do you want it?",
+        choices: @[Choice(title: kstring"Real bad", path: kstring"cruelty"),
+                   Choice(title: kstring"Ha-ha, I was just joking", path: kstring"joking")]
       )
   }.to_table
+  initScreen = questionScreens["welcome"]
 
-proc createDom*(): VNode =
+proc createDom*(rd: RouterData): VNode =
+  let key = if rd.hashPart == "": "welcome"
+            else: ($rd.hashPart)[1..<len(rd.hashPart)]
+  let screen = questionScreens.getOrDefault(key, initScreen)
   withLayout:
-    let scr = questionScreens["welcome"]
-    renderQuestion(scr.title)
-    renderChoices(scr.choices)
+    renderQuestion(screen.title)
+    renderChoices(screen.choices)
--- src/rendering.nim
+++ src/rendering.nim
@@ -1,4 +1,5 @@
 include karax / prelude
+import karax / kdom except class
 
 proc renderQuestion*(title: kstring): VNode =
   result = buildHtml(h1(class="title has-text-centered")):
@@ -8,12 +9,15 @@ type Choice* = object
   title*, path*: kstring
 proc renderChoices*(choices: openArray[Choice]): VNode =
   result = buildHtml(tdiv(class="buttons are-medium is-centered")):
-    for chs in choices:
-      a(class="button is-primary is-outlined", href="#"&chs.path):
-        text chs.title
+    for choice in choices:
+      a(class="button is-primary is-outlined", href="#"&choice.path):
+        text choice.title
+        proc onMouseUp(ev: Event, n: VNode) =
+          for btn in document.querySelectorAll(".buttons .button"):
+            btn.blur()
 
 template withLayout*(content: untyped) =
-  result = buildHtml(tdiv(class="hero is-dark is-fullheight")):
+  result = buildHtml(tdiv(class = "hero is-dark is-fullheight")):
     tdiv(class="hero-body"):
       tdiv(class="container"):
         content
