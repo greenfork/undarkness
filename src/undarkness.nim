e498314b0c49933b3cf24a3313a4495fa17987f4
Dmitry Matveyev <dm.matveyev@protonmail.com>
2019-09-11 21:30:01 +0300

    Remove main module compile condition

--- src/undarkness.nim
+++ src/undarkness.nim
@@ -2,9 +2,8 @@ from karax / karax import setRenderer
 
 from logic import createDom, initLogic
 
-when isMainModule:
-  proc main() =
-    initLogic()
-    setRenderer(createDom)
+proc main() =
+  initLogic()
+  setRenderer(createDom)
 
-  main()
+main()
